db99db9f0357d764adb2db358f3b4c05ec17387a
Александр Гавриленко <lihoy.sasha@gmail.com>
2021-09-05 13:06:35 +0300

    Добавленна игра Калькулятор

--- makefile
+++ makefile
@@ -6,5 +6,7 @@ brain-games:
 	./bin/brain-games
 brain-even:
 	./bin/brain-even
+brain-calc:
+	.bin/brain-even
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
---
+++ src/Calc.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Src\Calc;
+
+use function cli\line;
+use function cli\prompt;
+
+function main()
+{
+  $operations = ['*', '+', '-'];
+  line('Welcome to the Brain Game!');
+  $name = prompt('May I have your name?');
+  line("Hello, {$name}!");
+  line('What is the result of the expression?');
+  for ($i = 0; $i < 3; $i++) {
+    $numberOperation = rand(0, 2);
+    $operation = $operations[$numberOperation];
+    $digit1 = rand(0, 100);
+    $digit2 = rand(0, 100);
+    line("Question: {$digit1} {$operation} {$digit2}");
+    $answer = prompt('Your answer');
+    if ($operation === '+') {
+      if ($answer == $digit1 + $digit2) {
+        line('Correct!');
+      } else {
+        $correctAnswer = $digit1 + $digit2;
+        line("{$answer} is wrong answer ;(. Correct answer was {$correctAnswer}");
+        line("Let's try again, {$name}!");
+        return false;
+      }
+    }
+    if ($operation === '-') {
+      if ($answer == $digit1 - $digit2) {
+        line('Correct!');
+      } else {
+        $correctAnswer = $digit1 - $digit2;
+        line("{$answer} is wrong answer ;(. Correct answer was {$correctAnswer}");
+        line("Let's try again, {$name}!");
+        return false;
+      }
+    }
+    if ($operation === '*') {
+      if ($answer == $digit1 * $digit2) {
+        line('Correct!');
+      } else {
+        $correctAnswer = $digit1 * $digit2;
+        line("{$answer} is wrong answer ;(. Correct answer was {$correctAnswer}");
+        line("Let's try again, {$name}!");
+        return false;
+      }
+    }
+  }
+  line("Congratulations, {$name}!");
+}
